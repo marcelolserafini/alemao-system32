51cdc8a5e1c52d26767ab29b1008dc88d6fce8e7
marcelolserafini <marcelowet2@gmail.com>
2026-05-23 15:33:38 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Configuração do Firebase PWA - Isolado do Git via .gitignore para proteção
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyB5qH8g-kcMv4yGqk7Krm5D_vbV26X-7XU",
-    authDomain: "alemaozinho-system32.firebaseapp.com",
-    projectId: "alemaozinho-system32",
-    storageBucket: "alemaozinho-system32.firebasestorage.app",
-    messagingSenderId: "816402135407",
-    appId: "1:816402135407:web:ff9ee42560e2b8fef2109a"
-};
